dfdfd02b6d89387acbe306090bb3470ece04b302
ali moniri <alimoniri81@gmail.com>
2022-05-22 00:27:37 +0430

    finish side bar

--- js/app.js
+++ js/app.js
@@ -72,3 +72,34 @@ function connect() {
         });
     })
 }
+
+
+
+
+
+// dropdown ----------------------------
+
+$(function() {
+    var Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+
+        var items = this.el.find('.item');
+        items.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
+    }
+
+    Accordion.prototype.dropdown = function(e) {
+        var $el = e.data.el;
+        $this = $(this),
+            $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+            $el.find('.subItems').not($next).slideUp().parent().removeClass('open');
+        };
+    }
+
+    var accordion = new Accordion($('#accordion'), false);
+});
